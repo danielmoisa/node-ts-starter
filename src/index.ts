c71c2f5c9ae9d54695281d018a07833d247a0860
Daniel Moisa <dani93moisa@gmail.com>
2021-03-31 19:43:34 +0300

    configs

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
-import config from "./config";
+import { PORT } from "./config/app";
 
 dotenv.config();
 const prisma = new PrismaClient();
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Hellow");
+	res.send("Hello!");
 });
 
 app.get("/users", async (req, res) => {
@@ -26,4 +26,4 @@ app.post(`/user`, async (req, res) => {
 	res.json(result);
 });
 
-app.listen(config.port, () => console.log(`REST API server ready at: http://localhost:${config.port}`));
+app.listen(PORT, () => console.log(`REST API server ready at: http://localhost:${PORT}`));
